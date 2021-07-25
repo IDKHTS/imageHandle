ba09590ca7ca3ee75980bff92e2a354b6987e9b8
idkhts <2811212491@qq,com>
2021-07-25 17:39:13 +0800

    [modify]：重构图片压缩

---
+++ compress.js
@@ -0,0 +1,47 @@
+async function compress(file, options = { type: 'image/jpeg', quality: 0.8 }) {
+    const type = options.type
+    const quality = options.quality || 0.8
+
+    const fileData = await readFile(file)
+    const imgEl = await createImgEl(fileData)
+    const canvas = getCompressCanvas(imgEl)
+    return new Promise((resolve, reject) => {
+        canvas.toBlob((blob) => resolve(blob), type, quality)
+    })
+}
+
+function readFile(file) {
+    return new Promise((resolve, reject) => {
+        let reader = new FileReader()
+        reader.readAsDataURL(file)
+        reader.onload = function (data) {
+            console.log('FileReader', data)
+            resolve(reader.result)
+        }
+        reader.onerror = (err) => reject(err)
+    })
+}
+
+function createImgEl(fileData) {
+    return new Promise((resolve, reject) => {
+        const img = new Image()
+        img.src = fileData
+        img.onload = function (e) {
+            console.log(e)
+            resolve(img)
+        }
+        img.onerror = (err) => reject(err)
+    })
+}
+
+function getCompressCanvas(imgEl) {
+    const canvas = document.createElement('canvas')
+    const ctx = canvas.getContext('2d')
+
+    canvas.width = imgEl.naturalWidth
+    canvas.height = imgEl.naturalHeight
+
+    ctx.drawImage(imgEl, 0, 0, canvas.width, canvas.height)
+
+    return canvas
+}
