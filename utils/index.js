d886deba3b47f6d75f38614c3b67bbb942b93e82
idkhts <2811212491@qq,com>
2021-07-29 23:31:32 +0800

    [feature]: 工具函数添加下载函数

--- utils/index.js
+++ utils/index.js
@@ -24,15 +24,15 @@ function createImgEl(fileData) {
 
 // 创建tag标签，子元素是children，children若为字符串，则标签内容为这字符串
 function creatEL(tag, children) {
-    let el = document.createElement(tag);
+    let el = document.createElement(tag)
     if (children && Array.isArray(children)) {
-        children.forEach((v) => el.appendChild(v));
-    } else if (children && typeof children == "object") {
-        el.appendChild(children);
+        children.forEach((v) => el.appendChild(v))
+    } else if (children && typeof children == 'object') {
+        el.appendChild(children)
     } else if (children) {
-        el.innerHTML = children;
+        el.innerHTML = children
     }
-    return el;
+    return el
 }
 
 function throttle(delay, fn) {
@@ -43,33 +43,40 @@ function throttle(delay, fn) {
             cando = false
             setTimeout(() => {
                 cando = true
-            }, delay);
+            }, delay)
         }
     }
-
 }
-
 
 // ImageData获取x行y列颜色为color，obj为ImageData数据对象
 function getXY(obj, x, y) {
-    const w = obj.width;
+    const w = obj.width
     // var h = obj.height;
     // var d = obj.data;
-    let color = [];
-    color[0] = obj.data[4 * (y * w + x)];
-    color[1] = obj.data[4 * (y * w + x) + 1];
-    color[2] = obj.data[4 * (y * w + x) + 2];
-    color[3] = obj.data[4 * (y * w + x) + 3];
-    return color;
+    let color = []
+    color[0] = obj.data[4 * (y * w + x)]
+    color[1] = obj.data[4 * (y * w + x) + 1]
+    color[2] = obj.data[4 * (y * w + x) + 2]
+    color[3] = obj.data[4 * (y * w + x) + 3]
+    return color
 }
 
 // ImageData设置x行y列颜色为color，obj为ImageData数据对象
 function setXY(obj, x, y, color) {
-    var w = obj.width;
+    var w = obj.width
     // var h = obj.height;
     // var d = obj.data;
-    obj.data[4 * (y * w + x)] = color[0];
-    obj.data[4 * (y * w + x) + 1] = color[1];
-    obj.data[4 * (y * w + x) + 2] = color[2];
-    obj.data[4 * (y * w + x) + 3] = color[3];
+    obj.data[4 * (y * w + x)] = color[0]
+    obj.data[4 * (y * w + x) + 1] = color[1]
+    obj.data[4 * (y * w + x) + 2] = color[2]
+    obj.data[4 * (y * w + x) + 3] = color[3]
+}
+
+let num = 1
+function download(code, name) {
+    const download = document.createElement('a')
+    download.download = name || '截图' + num
+    num++
+    download.href = code
+    download.click()
 }
